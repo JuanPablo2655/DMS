3481a30693588010d87de462cdafcd4226c9564e
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2018-10-12 10:13:22 -0400

    getting the bot ready

--- dead.js
+++ dead.js
@@ -3,3 +3,17 @@ const dead = new Discord.Client;
 
 const config = require("./config.json");
 const secrets = require("./secrets.json");
+
+dead.on('ready', () => {
+    console.log("ready bois!");
+});
+
+dead.on('message', (message) => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+})
+
+dead.login(secrets.token);
