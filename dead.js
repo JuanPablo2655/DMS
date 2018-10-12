005357a103dd0072eb4149d29b3d33feb33220ff
Juan Pablo <34992574+JuanPablo2655@users.noreply.github.com>
2018-10-12 18:47:13 -0400

    added a lot of commands

--- dead.js
+++ dead.js
@@ -5,7 +5,9 @@ const config = require("./config.json");
 const secrets = require("./secrets.json");
 
 dead.on('ready', () => {
+    var channel = dead.channels.get('500438046630608920');
     console.log("ready bois!");
+    channel.send("bot online");
 });
 
 dead.on('message', (message) => {
@@ -14,6 +16,57 @@ dead.on('message', (message) => {
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-})
+
+    if (command === "pick") {
+        const answers = [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5'
+        ];
+        message.channel.send(`${answers[~~(Math.random() * answers.length)]}`);
+    };
+
+    if (command === "weather") {
+        const weather = [
+            'rainy',
+            'windy',
+            'cloudy',
+            'sunny',
+        ];
+        message.channel.send(`${weather[~~(Math.random() * weather.length)]}`);
+    };
+
+    if (command === "disaster") {
+        const disaster = [
+            'flood',
+            'tornado',
+            'earthquake',
+            'wild fires',
+        ];
+        message.channel.send(`${disaster[~~(Math.random() * disaster.length)]}`);
+    }
+
+    if (command === "horde") {
+        const horde = [
+            'extra small',
+            'small',
+            'medium',
+            'big',
+            'extra big',
+        ];
+        message.channel.send(`${horde[~~(Math.random() * horde.length)]}`);
+    };
+
+    if (command === "intruder") {
+        const intruder = [
+            'small gang',
+            'medium gang',
+            'large gang',
+        ];
+        message.channel.send(`${intruder[~~(Math.random() * intruder.length)]}`);
+    }
+});
 
 dead.login(secrets.token);
